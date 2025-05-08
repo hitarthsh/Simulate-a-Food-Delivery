690dc644845ca6b43011e25eb2172385cc4cfad7
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-05-08 15:26:06 +0530

    add after this code

---
+++ script2.js
@@ -0,0 +1,7 @@
+// ✅ 2. Chained Promises: User - Posts - Comments
+
+// Task:
+// 1. Create getUser() resolves with { id: 1, name: "Harsh" }
+// 2. getPosts(userId) - resolves with list of post titles
+// 3. getComments(postId) - resolves with comments
+// Chain them together using .then() and log the final output.
